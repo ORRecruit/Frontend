3dcb3a9030269650d00b3c1466cc1184a04cfdf1
zeeshan363 <zsarfraz363@gmail.com>
2024-02-06 21:38:10 +0500

    minor refactoring in assessment section

--- src/components/landing/assessments/assessments.tsx
+++ src/components/landing/assessments/assessments.tsx
@@ -19,74 +19,66 @@ const assessments: React.FC<AssessmentInterface> = ({
 }) => {
   return (
     <>
-      <div className="w-full mx-auto py-14 mt-20">
-        <div className="w-4/5 mx-auto 2xl:w-3/4">
-          <div className="flex justify-between items-center">
-            <div className="w-1/2 xl:w-w-58">
-              <h1 className="text-4xl font-bold">{heading1}</h1>
-              <p className="font-base font-normal my-3 w-11/12 text-text-gray xl:my-5 2xl:my-6">
-                {paragraph1}
-              </p>
-              <button
-                type="button"
-                className="w-36 h-10 flex justify-between items-center text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center bg-color-blue my-3"
+      <section className="bg-white">
+        <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+          <img src={img1} alt="image-1" />
+          <div className="mt-4 md:mt-0">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-black">
+              {heading1}
+            </h2>
+            <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
+              {paragraph1}
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center text-white bg-color-blue focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Get started
+              <svg
+                className="ml-2 -mr-1 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Get started
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2 text-base font-medium"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="w-1/2 xl:w-2/5">
-              <img src={img1} alt="" />
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <div className="w-1/2 xl:w-2/5">
-              <img src={img2} alt="" />
-            </div>
-            <div className="w-1/2 xl:w-w-58">
-              <h1 className="text-4xl font-bold">{heading2}</h1>
-              <p className="font-base font-normal my-3 w-11/12 text-text-gray xl:my-5 2xl:my-6">
-                {paragraph2}
-              </p>
-              <button
-                type="button"
-                className="w-36 h-10 flex justify-between items-center text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center bg-color-blue my-3"
-              >
-                Get started
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2 text-base font-medium"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
-      </div>
+        <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+          <div className="mt-4 md:mt-0">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-black">
+              {heading2}
+            </h2>
+            <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
+              {paragraph2}
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center text-white bg-color-blue focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Get started
+              <svg
+                className="ml-2 -mr-1 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <img src={img2} alt="image-2" />
+        </div>
+      </section>
     </>
   );
 };
