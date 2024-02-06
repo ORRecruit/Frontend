bd13a55a39017692e380deb50cdb845e9e9e01dc
zeeshan363 <zsarfraz363@gmail.com>
2024-02-06 22:13:12 +0500

    modified footer and about section

--- src/components/landing/about/about.tsx
+++ src/components/landing/about/about.tsx
@@ -12,10 +12,10 @@ const about: React.FC<aboutInterface> = ({ height, backgroundImage }) => {
   return (
     <div style={divStyle} className="bg-about w-full">
       <div className="w-4/6 mx-auto h-full flex flex-col justify-center items-center xl:w-2/5">
-        <p className="text-4xl	font-bold text-white my-3">
+        <p className="text-3xl font-bold text-white my-3">
           Get started with ORR
         </p>
-        <p className="text-gray-500 text-xl font-normal text-center my-3">
+        <p className="text-gray-500 text-lg font-normal text-center my-3">
           Find your perfect talent waiting to meet you and show you what they
           have got from thousands miles away
         </p>
--- src/components/landing/footer/footer.tsx
+++ src/components/landing/footer/footer.tsx
@@ -2,27 +2,62 @@ import React from "react";
 
 export const footer = () => {
   return (
-    <div className="bg-footer h-footer w-full">
-      <div className="w-4/5 mx-auto h-full flex flex-col justify-center items-center xl:w-4/6">
-        <img className="my-3 w-16 h-16" src="/ORR-logo.svg" alt="" />
-        <p className="text-center my-3 text-xl	font-normal text-gray-400">
-          4k+ Recruiters with 50k+ Talented individuals. ORR thrives on its on
-          point AI based precise recruitment process. It’s tailored to modern Ai
-          needs.{" "}
-        </p>
-        <div className="lg:w-1/2 flex justify-between my-3 text-base	font-normal w-full">
-          <a href="#">Jobs</a>
-          <a href="#">Recruiters</a>
-          <a href="#">Talent</a>
-          <a href="#">Blog</a>
-          <a href="#">About</a>
-          <a href="#">How It Works</a>
+    <>
+      <footer className="p-4 bg-white md:p-8 lg:p-10">
+        <div className="mx-auto max-w-screen-xl text-center">
+          <a
+            href="#"
+            className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img className="my-3 w-16 h-16" src="/ORR-logo.svg" alt="" />
+          </a>
+          <p className="my-6 text-gray-500 dark:text-gray-400">
+            4k+ Recruiters with 50k+ Talented individuals. ORR thrives on its on
+            point AI based precise recruitment process. It’s tailored to modern
+            Ai needs.
+          </p>
+          <ul className="flex flex-wrap justify-center items-center mb-6">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                Jobs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Recruiters
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                Talent
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                How It Works
+              </a>
+            </li>
+          </ul>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2022{" "}
+            <a href="#" className="hover:underline">
+              ORR, Inc
+            </a>
+            . All Rights Reserved.
+          </span>
         </div>
-        <p className="my-3 text-gray-400">
-          © 2022 ORR, Inc. All rights reserved.
-        </p>
-      </div>
-    </div>
+      </footer>
+    </>
   );
 };
 
