7b50d47ea4faf250d46bc2bbb9a4069ce7c994cf
zeeshan363 <zsarfraz363@gmail.com>
2024-02-07 22:04:23 +0500

    changes in navbar

--- src/components/landing/jobs/page.tsx
+++ src/components/landing/jobs/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
   ];
   return (
     <>
-      <div className="w-full mx-auto bg-bg-service py-28 mt-20">
+      <div className="w-full mx-auto bg-bg-service py-28 mt-20 2xl:w-11/12">
         <div className="w-4/5 mx-auto 2xl:w-4/5">
           <div className="mb-12">
             <p className="text-3xl	font-bold mb-2">How it Works</p>
--- src/components/landing/navbar/navbar.tsx
+++ src/components/landing/navbar/navbar.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-const navbar = () => {
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="bg-white h-24 lg:w-4/5 mx-auto">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 lg:">
@@ -25,35 +32,38 @@ const navbar = () => {
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-cta"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen ? "true" : "false"}
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 17 14"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
             >
               <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
           </button>
         </div>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 lg:absolute lg:left-1/4"
+          className={`${
+            isMenuOpen
+              ? "flex flex-col bg-gray-200 z-10 rounded-xl mt-2.5 text-white"
+              : "hidden"
+          } items-center justify-between w-full md:flex md:w-auto md:order-1 lg:absolute lg:left-1/4`}
           id="navbar-cta"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
+          <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <a
                 href="/landing/jobs"
-                className="block py-2 px-3 md:p-0 text-black rounded"
+                className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0"
                 aria-current="page"
               >
                 Jobs
@@ -62,7 +72,7 @@ const navbar = () => {
             <li>
               <a
                 href="/landing/recruiters"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent dark:text-black dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0"
               >
                 Recruiters
               </a>
@@ -70,15 +80,15 @@ const navbar = () => {
             <li>
               <a
                 href="/landing/blogs"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-black dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0"
               >
                 Blog
               </a>
             </li>
             <li>
               <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent dark:text-black dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent dark:border-gray-700"
+                href="/landing/about"
+                className="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0"
               >
                 About
               </a>
@@ -90,4 +100,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
