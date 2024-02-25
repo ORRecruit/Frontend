9d3bd65f8840916e6d94c311170677f67ea3fd7e
daniishkhan <danishafzalkhan@gmail.com>
2024-02-25 22:29:39 +0500

    fixed content and layout on jobs page

--- src/app/(landing)/jobs/page.tsx
+++ src/app/(landing)/jobs/page.tsx
@@ -5,6 +5,8 @@ import StatsComponent from "../../../components/landing/jobs/statsComponent/stat
 import Assessments from "../../../components/landing/assessments/assessments";
 import Footer from "../../../components/landing/footer/footer";
 import About from "../../../components/landing/about/about";
+import Features from "@/components/landing/jobs/features";
+import ContactUs from "@/components/landing/contactUs/contactUs";
 
 const page = () => {
   const iconImages = [
@@ -18,29 +20,23 @@ const page = () => {
     <>
       <Navbar />
       <HeroSection
-        heading={"Match with top Canadian Firms"}
-        paragraph={`Experience a guided process that helps you create a resume reflecting your true professional self.`}
-        videoImageSrc={`/hero-section-img-job.svg`}
+        heading={"Find your Dream Remote Job Today!"}
+        paragraph={`Experience a guided process that helps you land a job that propels your career forward.`}
+        videoImageSrc={`/tracher-img.svg`}
         iconImages={iconImages}
         showIconImages="false"
       />
-      <StatsComponent />
+        <Features />
+      {/*<StatsComponent />*/}
       <Assessments
-        heading1={`Hire Quality Talent using AI`}
+        heading1={`Find jobs that match your career tragetory`}
         paragraph1={`Utilize our AI-driven skill assessment tools to gain insights into your strengths and areas for growth. Our platform provides personalized suggestions for skill development, helping you stay competitive and ready for new challenges.`}
-        img1={`/assessment-page-img1.svg`}
-        heading2={`Application Tracker`}
+        img1={`/assessment-img.svg`}
+        heading2={`Shortlist jobs using keywords and smart guides`}
         paragraph2={`With our real-time application tracking system, you’ll always know the status of your job applications. Get instant updates and feedback, so you’re never left wondering where you stand.`}
         img2={`/assessment-page-img2.svg`}
       />
-      <Assessments
-        heading1={`Hire Quality Talent using AI`}
-        paragraph1={`Join our community of like-minded professionals. Participate in discussions, share experiences, and gain insights from others who have navigated the job-seeking journey. At Online Remote Recruiting, you're not just finding a job; you're joining a community.`}
-        img1={`/assessment-page-img3.svg`}
-        heading2={`Smart Job Matching`}
-        paragraph2={`No more endless scrolling through irrelevant job listings. Our AI analyzes your profile and preferences to bring you job opportunities that genuinely fit your skills and career aspirations. Realize your worth in a marketplace that values your unique capabilities.`}
-        img2={`/assessment-page-img4.svg`}
-      />
+        <ContactUs />
       <About height="473px" backgroundImage="" />
       <Footer />
     </>
