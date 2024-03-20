abad1dfef1a39b91fc13ac59da93267c63f02581
zeeshan363 <zsarfraz363@gmail.com>
2024-03-20 06:55:32 +0500

    started work with admin Dashboard

--- src/app/dashboard/talentDashboard/overview/page.tsx
+++ src/app/dashboard/talentDashboard/overview/page.tsx
@@ -4,12 +4,34 @@ import DashboardSidebar from "@/components/dashboard/dashboardSidebar/dashboardS
 import React from "react";
 
 const overview = () => {
+  const details = [
+    {
+      iconUrl: "/overview1.svg",
+      heading: "Jobs Applied",
+      rating: "$163.4k",
+    },
+    {
+      iconUrl: "/overview2.svg",
+      heading: "AI Searches",
+      rating: "$82.1k",
+    },
+    {
+      iconUrl: "/overview3.svg",
+      heading: "Profile Views",
+      rating: "$81.3k",
+    },
+    {
+      iconUrl: "/overview4.svg",
+      heading: "Interviews",
+      rating: "68",
+    },
+  ];
   return (
     <div className="bg-dashboard h-screen w-full">
       <DashboardNavbar />
       <div className="flex">
         <DashboardSidebar />
-        <DashboardContent />
+        <DashboardContent details={details} />
       </div>
     </div>
   );
