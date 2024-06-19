1fa5b032b4594ff9e6515c494fbcaaa0e6711e60
zeeSHAN Sarfraz <zsarfraz363@gmail.com>
2024-06-19 20:28:41 +0500

    Fix: hot fixes in talent and admin panel

--- src/app/admin/dashboard/jobBoard/ai-matching/page.tsx
+++ src/app/admin/dashboard/jobBoard/ai-matching/page.tsx
@@ -237,15 +237,18 @@ const page = () => {
                                           {selectedItem?.fullName}
                                         </h1>
                                       </div>
-                                      <div className="font-light text-lg font-semibold text-gray-500 dark:text-gray-400">
+                                      <div className="font-semibold text-lg text-gray-500 dark:text-gray-400">
                                         ORR-
                                         {selectedItem?.industry?.slice(0, 4)}
                                         -00
                                         {selectedItem?.id}
                                       </div>
 
-                                      <span className="inline-block bg-green-200 text-green-800 text-xs px-2 rounded">
+                                      <span className="font-light text-gray-500 dark:text-gray-400">
                                         {selectedItem?.email}
+                                      </span>
+                                      <span className="font-light text-gray-500 dark:text-gray-400">
+                                        {selectedItem?.industry}
                                       </span>
                                     </div>
 
@@ -257,13 +260,40 @@ const page = () => {
                                         {selectedItem?.location}
                                       </p>
 
-                                      <p className="text-lg font-extrabold text-gray-900 dark:text-white">
+                                      <div>
+                                        <h1 className="font-semibold text-lg text-gray-500 dark:text-gray-400 pt-2">
+                                          SKills
+                                        </h1>
                                         {selectedItem?.skills?.map(
-                                          (item: any) => {
-                                            <span>{item}</span>;
+                                          (item: any, index: number) => {
+                                            return (
+                                              <span key={index}>{item}, </span>
+                                            );
                                           }
                                         )}
-                                      </p>
+                                      </div>
+
+                                      <div className="text-gray-900 dark:text-white">
+                                        <h1 className="font-semibold text-lg text-gray-500 dark:text-gray-400 pt-2">
+                                          Tech
+                                        </h1>
+                                        {selectedItem?.tools?.map(
+                                          (item: any, index: number) => {
+                                            return (
+                                              <span key={index}>{item}, </span>
+                                            );
+                                          }
+                                        )}
+                                      </div>
+
+                                      <div className="text-gray-900 dark:text-white">
+                                        <h1 className="font-semibold text-lg text-gray-500 dark:text-gray-400 pt-2">
+                                          About Me
+                                        </h1>
+                                        <span className="font-light text-gray-500 dark:text-gray-400">
+                                          {selectedItem?.about}
+                                        </span>
+                                      </div>
                                     </div>
                                   </div>
                                   <button
@@ -314,47 +344,6 @@ const page = () => {
                         clipRule="evenodd"
                       />
                     </svg>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    1
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    2
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    aria-current="page"
-                    className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                  >
-                    3
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    ...
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    100
                   </a>
                 </li>
                 <li>
