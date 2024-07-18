d03aa5c53d1bae8d718fb9cb6943bcbb2ed61222
zeeSHAN Sarfraz <zsarfraz363@gmail.com>
2024-07-19 02:23:58 +0500

    apollo integration

--- src/components/custom/GoogleTagManager.tsx
+++ src/components/custom/GoogleTagManager.tsx
@@ -9,17 +9,6 @@ const GoogleTagManager: FC<Props> = ({ containerId }) => {
   return (
     <>
       <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WQBFH7L6');`,
-        }}
-      />
-      <Script
         id="apollo-tracker"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
