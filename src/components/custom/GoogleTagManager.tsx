18326e05716cffecae198e2107a33c43969ab557
zeeSHAN Sarfraz <zsarfraz363@gmail.com>
2024-07-19 02:17:48 +0500

    apollo script

--- src/components/custom/GoogleTagManager.tsx
+++ src/components/custom/GoogleTagManager.tsx
@@ -19,6 +19,26 @@ const GoogleTagManager: FC<Props> = ({ containerId }) => {
             })(window,document,'script','dataLayer','GTM-WQBFH7L6');`,
         }}
       />
+      <Script
+        id="apollo-tracker"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      function initApollo() {
+        var n = Math.random().toString(36).substring(7);
+        var o = document.createElement("script");
+        o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+        o.async = true;
+        o.defer = true;
+        o.onload = function() {
+          window.trackingFunctions.onLoad({appId: "667d8edb4b466e0a9f2e49a4"});
+        };
+        document.head.appendChild(o);
+      }
+      initApollo();
+    `,
+        }}
+      />{" "}
     </>
   );
 };
