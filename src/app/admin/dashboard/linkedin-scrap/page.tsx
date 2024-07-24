8ee186837d1c7c4ff71ba81f3d188b18493eb98d
zeeSHAN Sarfraz <zsarfraz363@gmail.com>
2024-07-25 04:38:24 +0500

    Feat: geoIds from constant folder

--- src/app/admin/dashboard/linkedin-scrap/page.tsx
+++ src/app/admin/dashboard/linkedin-scrap/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import ResumeTemplate from "@/components/templates/ResumeTemplate";
 import { pdf } from "@react-pdf/renderer";
 import saveAs from "file-saver";
+import { geoIds } from "@/constants/geoIds";
 
 const Page = () => {
   const toggleMenu = useToggleStore((state) => state.isSidebarOpen);
@@ -142,18 +143,23 @@ const Page = () => {
               </div>
               <div className="w-[32%] mr-2">
                 <label className="block mb-1 mt-2 text-sm font-medium text-gray-500 dark:text-white">
-                  GeoId*
+                  Geoid
                 </label>
-                <input
-                  type="number"
-                  name="geoId"
+                <select
                   id="geoId"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="GeoId"
+                  name="geoId"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   value={formData.geoId}
                   onChange={handleChange}
-                  required={true}
-                />
+                >
+                  <option value="temporary">Select geoId</option>
+                  {geoIds &&
+                    geoIds.map((item: any, index: any) => (
+                      <option key={index} value={item?.geoId}>
+                        {item?.location}
+                      </option>
+                    ))}
+                </select>
               </div>
               <div className="w-[32%] mr-2">
                 <label className="block mb-1 mt-2 text-sm font-medium text-gray-500 dark:text-white">
