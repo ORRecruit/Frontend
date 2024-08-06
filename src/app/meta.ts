fcafcf6bf483f8b00bc3fddac5040265fec752b2
zeeSHAN Sarfraz <zsarfraz363@gmail.com>
2024-08-06 13:55:36 +0500

    google search console

--- src/app/meta.ts
+++ src/app/meta.ts
@@ -2,6 +2,6 @@
 export const metadata = {
   title: "Online Remote Recruiting",
   verification: {
-    google: "3cgj6fLT8zsNQ9EbAU5zTZteKVBAHXSiZR3-VNgB7gk",
+    google: "3T9gCJ7uValTT8ZlgCzp0sfFuJ2-q7BZ5Y_VKu7Dq2M",
   },
 };
